a8725a9602df1539ce52ea176ecec13492b5de24
omphalosskeptic <isa@automathic.org>
2013-09-28 19:47:03 -0700

    Update dg_paths.php
    You have the default path using the old name, 'datagrid', instead of what folks get when they download it now — 'Datagridle'. This fixes it.

--- dg_paths.php
+++ dg_paths.php
@@ -3,9 +3,9 @@
 // Use absolute paths for client-side files (CSS and JS) to enable grids at different levels of client side depth
 
 $this->paths = array(
-   css_path => '/common/libraries/datagrid/css/',
-   js_path => '/common/libraries/datagrid/js/',
-   tiny_mce_path => '/common/libraries/datagrid/js/tiny_mce/',
+   css_path => '/common/libraries/Datagridle/css/',
+   js_path => '/common/libraries/Datagridle/js/',
+   tiny_mce_path => '/common/libraries/Datagridle/js/tiny_mce/',
    classes_path => '../../../'
 );
 
